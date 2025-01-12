b32041b2c9bf983b9e9ecd6794dfc39e68353332
DragosIlies <ilies9dragos@gmail.com>
2025-01-12 21:56:19 +0000

    added start button and hover functionality with tailwind

--- src/Components/Taskbar/Taskbar.tsx
+++ src/Components/Taskbar/Taskbar.tsx
@@ -1,7 +1,13 @@
 export function Taskbar() {
     return (
-    <div className="row-start-2 row-end-3 col-start-1 col-end-3">
-        Taskbar
-    </div>
+    <footer 
+        className="row-start-2 row-end-3 col-start-1 col-end-3 bg-taskbar-colour flex">
+        <div className="bg-start-button-norm hover:bg-start-button-hover bg-no-repeat  w-[97px] h-[30px]">
+            
+        </div>
+        <div>
+            
+        </div>
+    </footer>
     )
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'taskbar-colour': 'linear-gradient(#1f2f86, #3165c4 3%, #3682e5 6%, #4490e6 10%, #3883e5 12%, #2b71e0 15%, #2663da 18%, #235bd6 20%, #2258d5 23%, #2157d6 38%, #245ddb 54%, #2562df 86%, #245fdc 89%, #2158d4 92%, #1d4ec0 95%, #1941a5 98%)',
+        'start-button-norm': "url('assets/start-button/xp_btn_norm.png')",
+        'start-button-hover': "url('assets/start-button/xp_btn_hover.png')",
+        'start-button-click': "url('assets/start-button/xp_btn_click.png')",
+      },
+    },
   },
   plugins: [],
 }
