541802ecdc560fd06abdca292ed77a2038ca232d
DragosIlies <ilies9dragos@gmail.com>
2025-01-12 15:11:46 +0000

    setup special windows for being on top of everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css'
 import { ListShortcuts } from './Components/Shortcuts/ListShortcuts'
 import { Taskbar } from './Components/Taskbar/Taskbar'
-import { ListAppWindow } from './Components/Windows/ListAppWindow'
+// import { ListAppWindow } from './Components/Windows/ListAppWindow'
 
 function App() {
   return (
-    <section>
-      <div className="grid gap-2 grid-rows-[1fr_auto] grid_cols-[auto_1fr] h-screen w-full">
+    <section className = "h-screen w-full">
+      <div className="grid gap-2 grid-rows-[1fr_auto] grid_cols-[auto_1fr] h-full w-full">
         <ListShortcuts />
         <Taskbar />
       </div>
-      <ListAppWindow />
+      {/* <ListAppWindow /> */}
     </section>
   )
 }
--- src/Components/Windows/ListAppWindow.tsx
+++ src/Components/Windows/ListAppWindow.tsx
@@ -1,7 +1,7 @@
 import { AppWindow } from "./AppWindow"
 
 export function ListAppWindow() {
-    return (<div>
+    return (<div className="absolute top-10 left-10 w-96 h-64 bg-white shadow-lg border border-gray-300">
         <AppWindow />
         <AppWindow />
         </div>)
