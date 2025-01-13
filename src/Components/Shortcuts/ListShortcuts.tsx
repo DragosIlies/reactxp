13e8eb97b6f5f919c5328712b1cbdc661441a925
DragosIlies <ilies9dragos@gmail.com>
2025-01-13 22:30:33 +0000

    aligned shortcuts

--- src/Components/Shortcuts/ListShortcuts.tsx
+++ src/Components/Shortcuts/ListShortcuts.tsx
@@ -2,7 +2,8 @@ import { Shortcut } from "./Shortcut"
 import computerIcon from "../../assets/app_icons/mycomputer.png"
 
 export function ListShortcuts() {
-    return (<div className="flex gap-2 p-2">
+    return (<div className="flex flex-col gap-6 p-8 items-start">
     <Shortcut title="My Computer" icon={computerIcon} altText="Computer Icon"/>
+    <Shortcut title="Work" icon={computerIcon} altText="Computer Icon"/>
     </div>)
   }
--- src/Components/Shortcuts/Shortcut.tsx
+++ src/Components/Shortcuts/Shortcut.tsx
@@ -1,10 +1,10 @@
 export function Shortcut({ title ,icon, altText }: { title: string, icon: string; altText: string }){
-    return <div className="flex flex-col items-center">
+    return <button className="flex flex-col items-center gap-1 w-20">
             <img
                 className="size-8"
                 src={icon}
                 alt={altText}
                 ></img>
-            <div className="text-white text-shadow-shortcut"> {title} </div>
-        </div>
+            <div className="text-white text-xs text-shadow-shortcut"> {title} </div>
+        </button>
 }
